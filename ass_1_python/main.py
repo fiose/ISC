7fe6767f8c811af1d77905d004d69d0d23ac4aaa
fiose <Saela2001>
2022-02-15 13:25:03 +0100

    getting sums of row and columns

--- ass_1_python/main.py
+++ ass_1_python/main.py
@@ -1,5 +1,6 @@
 import cv2
 import sys
+import numpy as np
 
 image_name = 'oval'
 image_path = 'Practical_Tomography/' + image_name + '.png'
@@ -9,3 +10,13 @@ A = cv2.imread(image_path)
 (width, height, _) = A.shape
 if not width == height:
     sys.exit(1)
+
+print(A)
+# get sums of columns and rows
+# todo: check if vertical and horizontal are correct
+colsum = np.sum(A, axis=0)  # vertical
+rowsum = np.sum(A, axis=1)  # horizontal
+print(colsum)
+print(rowsum)
+
+
