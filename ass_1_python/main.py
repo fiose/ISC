de975db5152f181bbe0c5e3f254183d4d345fa20
fiose <Saela2001>
2022-02-15 13:20:54 +0100

    checking if image is square

--- ass_1_python/main.py
+++ ass_1_python/main.py
@@ -1,5 +1,11 @@
 import cv2
+import sys
 
 image_name = 'oval'
 image_path = 'Practical_Tomography/' + image_name + '.png'
 A = cv2.imread(image_path)
+
+# check if image is square
+(width, height, _) = A.shape
+if not width == height:
+    sys.exit(1)
